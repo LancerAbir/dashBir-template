9340fc5b20a762e3142f86a309ad8a89e8576ff5
LancerAbir <anjan.o24@gmail.com>
2022-03-15 12:03:05 +0600

    fdsf

---
+++ dashboard menu and submenu list/dashboard.js
@@ -0,0 +1,201 @@
+Uncommon
+-----------
+(Main)
+Dashboard -->
+	1. Home
+
+
+Common
+----------
+(UI KIT)
+Apps -->
+	# Calendar
+		## Default Calendar
+		## Full Calendar
+	# Chat
+	# Mailbox
+	# Ecommerce
+		## Products
+		## Product Details
+		## Create Product
+		## Orders
+		## Order Details
+		## Customers
+		## Shopping Cart
+		## Checkout
+		## Sellers
+		## Seller Details
+
+	# Projects
+		## List
+		## Overview
+		## Create Project
+	# Tasks
+		## Kanban Board
+		## List View
+		## Task Details
+	# CRM
+		## Contacts
+		## Companies
+		## Deals
+		## Leads
+	# Crypto
+		## Transactions
+		## Buy & Sell
+		## Orders
+		## My Wallet
+		## ICO List
+		## KYC Application
+	# Invoices
+		## List View
+		## Details
+		## Create Invoice
+	# Support Tickets
+		## List View
+		## Ticket Details
+
+Bootstrap -->
+Tailwind CSS -->
+Authentication -->
+	# Sign In
+		## Basic
+		## Cover
+	# Sign Up
+		## Basic
+		## Cover
+	# Password Reset
+		## Basic
+		## Cover
+	# Lock Screen
+		## Basic
+		## Cover
+	# Logout
+		## Basic
+		## Cover
+	# Success Message
+		## Basic
+		## Cover
+	# Two Step Verification
+		## Basic
+		## Cover
+	# Errors
+		## 404 Basic
+		## 404 Cover
+		## 404 Alt
+		## 500
+
+Pages --> 
+    # Starter
+    # Profile
+		# Profile page
+		# Profile Settings
+    # Team
+    # Timeline
+    # FAQs
+    # Pricing
+    # Gallery
+    # Maintenance
+    # Coming Soon
+    # Sitemap
+    # Search Results
+
+Web Site -->
+Base UI --> 
+	# Alerts
+	# Badges
+	# Buttons
+	# Colors
+	# Cards
+	# Carousel
+	# Dropdowns
+	# Grid
+	# Images
+	# Tabs
+	# Accordion & Collapse
+	# Modals
+	# Offcanvas
+	# Placeholders
+	# Progress
+	# Notifications
+	# Media object
+	# Embed Video
+	# Typography
+	# Lists
+	# General
+	# Ribbons
+	# Utilities
+
+
+
+Advance UI --> 
+	# Sweet Alerts
+	# Nestable List
+	# Scrollbar
+	# Animation
+	# Tour
+	# Swiper Slider
+	# Ratings
+	# Highlight
+	# ScrollSpy
+widgets -->
+Forms -->
+	# Basic Elements
+	# Form Select
+	# Checkboxs & Radios
+	# Pickers
+	# Input Masks
+	# Advanced
+	# Range Slider
+	# Validation
+	# Wizard
+	# Editors
+	# File Uploads
+	# Form Layouts
+
+
+Table --> 
+	1. basic
+	2. listjs -- add row table
+charts -->
+	1. apex charts 
+		#. line
+		#. area
+		#. column
+		#. bar
+		#. mixed
+		#. timeline
+		#. candlestick
+		#. boxplot
+		#. bubble
+		#. scatter
+		#. heatmap
+		#. treemap
+		#. pie
+		#. radialbar
+		#. radar
+		#. polara-area
+       
+	2. chartjs
+	3. echarts
+Icoon -->
+	1. remix
+	2. boxicons
+	3. materialdesign
+	4. lineawesome
+	5. font-awesome
+	6. feather
+maps --> 
+	1. google
+	2. vector
+	3. leaflet
+
+
+
+
+	https://themesbrand.com/velzon/html/default/apps-tasks-details.html
+
+	https://spruko.com/demo/sash/sash/html/rangeslider.html
+
+	https://demo.cork-vue.sbthemes.com/apps/mailbox
+
+	https://pages.revox.io/dashboard/latest/html/condensed/email.html#
---
+++ dashboard menu and submenu list/todo.js
@@ -0,0 +1,2 @@
+1. show code option
+2. advance email option -->https://demo.cork-vue.sbthemes.com/apps/mailbox
